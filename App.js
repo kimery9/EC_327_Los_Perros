80d40128cf3199ba435f902745024c7b9ce0f4bd
kimery9 <114025961+kimery9@users.noreply.github.com>
2022-12-01 13:33:50 -0500

    Chart creation
    Chart is created by using ApexCharts.js and example is used using polygon.io with APPL ticker

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import {useEffect, useState} from 'react';
 import React from 'react'
 import {CssBaseline} from "@mui/material";
 import {Grid} from "@mui/material";
@@ -6,8 +7,71 @@ import Aggregate from "./components/Aggregate/Aggregate";
 import Details from "./components/Details/Details";
 import List from "./components/List/List";
 import News from "./components/News/News";
+import axios from 'axios';
+import Chart from 'react-apexcharts';
 
-const App = () => {
+//TODO stock is the correct ticker
+const apiUrl='https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2021-07-21/2022-07-22?adjusted=true&sort=asc&limit=500&apiKey=QVcqDoOgvanAJx0Tjpj01BtguV0OLYoA';
+async function getStocks(){
+ const response=await fetch(apiUrl);
+return response.json()
+}
+const chart= {
+    options: {
+        chart: {
+            type: 'candlestick',
+            height: 500
+        },
+        title: {
+            text: 'CandleStick Chart',
+            align: 'left'
+        },
+        xaxis: {
+            type: 'datetime'
+        },
+        yaxis: {
+            tooltip: {
+                enabled: true
+            }
+        }
+    },
+};
+
+
+function App() {
+    const [series, setSeries]=useState( [{
+        data: []
+    }]);
+    const [price, setPrice]=useState(-1);
+    const [pastPrice, setPastPrice]=useState(-1);
+    const [priceTime,setPriceTime]=useState(null);
+
+
+    useEffect(()=>{
+        getStocks()
+            .then((data)=>{
+                console.log(data);
+                const length= data.results.length;
+                console.log(length);
+                const historical=[];
+                for(let i=0; i<length; i++){
+                    const tix=data.results[i];
+                    historical[i]=[tix.t,tix.o,tix.h,tix.l,tix.c];
+                }
+
+                // const tix=data.results[0];
+                // console.log(tix);
+                // setPrice(tix.vw);
+                // setPriceTime(new Date(tix.t*1000));
+                // const plotprices=historical.map((t,index)=>({
+                //     x: new Date(t*1000),
+                //     y:[tix.o[index],tix.h[index],tix.l[index],tix.c[index]]
+                // }));
+                setSeries([{
+                     data:historical
+                 }])
+            })
+    },[]);
   return (
 <>
   <CssBaseline/>
@@ -34,6 +98,14 @@ const App = () => {
         heightValue={2}
     ></Divider>
     <Aggregate/>
+<div className="price">
+    {price}
+    <br/>
+    {priceTime && priceTime.toLocaleDateString()}
+</div>
+    <div>
+        <Chart options={chart.options} series={series} type="candlestick" width="100%" height={320} />
+    </div>
     <Divider
         primaryColor="red"
         secondaryColor="grey"
